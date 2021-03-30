626c19fb8b9253574c257e7098884b639943a3f7
svtter <svtter@qq.com>
2021-03-30 22:54:48 +0800

    feat: add commit message check

--- main.go
+++ main.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"log"
+	"strings"
 
 	"github.com/svtter/commit/pkg"
+)
+
+var (
+	starts = map[string]string {
+		"feat": "new feature",
+		"fix": "fix bug",
+		"docs": "documentation",
+		"style": "code style without business change",
+		"refactor": "refactor code",
+		"test": "test code",
+		"chore": "change for build tools",
+	}
 )
 
 func main() {
@@ -18,10 +31,22 @@ func main() {
 	} else {
 		commitArgs = pkg.ReadFromCommand()
 	}
+	if !checkPrefix(commitArgs) {
+		log.Println("commit message is not allowed. Please input with fea/fix/docs/style/refactor/test/chore.")
+	}
 
 	errout, out, err = pkg.Shellout("git", "commit", "-m", commitArgs)
 	pkg.Output(out, errout, err)
 
 	errout, out, err = pkg.Shellout("git", "push")
 	pkg.Output(out, errout, err)
+}
+
+func checkPrefix(args string) bool {
+	for key, _ := range starts {
+		if strings.HasPrefix(key, args) {
+			return true
+		}
+	}
+	return false
 }
