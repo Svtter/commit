d6e942f5c290693abdc96853ab3cca8794244988
修昊 <svtter@qq.com>
2021-04-06 14:42:50 +0800

    refact: update shelloutput

--- pkg/shell_test.go
+++ pkg/shell_test.go
@@ -51,9 +51,7 @@ func TestReadFromCommand(t *testing.T) {
 
 func TestOutputError(t *testing.T) {
 	type args struct {
-		out    string
-		errout string
-		err    error
+		r RunResult
 	}
 	tests := []struct {
 		name string
@@ -63,16 +61,14 @@ func TestOutputError(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			OutputError(tt.args.out, tt.args.errout, tt.args.err)
+			tt.args.r.OutputError()
 		})
 	}
 }
 
 func TestOutput(t *testing.T) {
 	type args struct {
-		out    string
-		errout string
-		err    error
+		r RunResult
 	}
 	tests := []struct {
 		name string
@@ -82,7 +78,7 @@ func TestOutput(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			Output(tt.args.out, tt.args.errout, tt.args.err)
+			tt.args.r.Output()
 		})
 	}
 }
